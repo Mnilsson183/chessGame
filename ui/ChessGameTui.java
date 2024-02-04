0fc3f74502d8c9ff3dfb6004fafc45cab95d25c8
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2024-02-04 00:14:06 -0700

    tui printing upgrade

--- ui/ChessGameTui.java
+++ ui/ChessGameTui.java
@@ -8,14 +8,25 @@ public class ChessGameTui extends ChessGameUi{
     }
 
     public void renderChessBoard(Board board){
+		int boardMaxRow = board.getBoardMaxRow();
+		int boardMaxColumn = board.getBoardMaxColumn();
+		char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 		System.out.println("Black");
 		for(int row = 0; row < board.getBoardMaxRow(); row++){
+			System.out.printf("%c|", alpha[boardMaxRow - row - 1]);
 			for(int y = 0; y < board.getBoardMaxColumn(); y++){
 				System.out.printf("%c ", board.getPiece(row, y).getType());
 			}
 			System.out.println();
 		}
-		System.out.println("White");
+		for(int i = 0; i < boardMaxColumn; i++){
+			System.out.printf("--");
+		}
+		System.out.println();
+		for(int i = 0; i < boardMaxColumn; i++){
+			System.out.printf("%d ", i + 1);
+		}
+		System.out.printf("\nWhite\n");
 		System.out.println();
 	}
 
